24ff895f03fd8900a58958d21668605c6bd74d63
Hosam <hosam5553@gmail.com>
2023-04-29 02:45:41 +0300

    separated styling and scripting to their correspondings

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const terms = [];
+const term = document.querySelector('#term');
+const results = document.querySelector('#results');
+
+(async function getTerms() {
+  return await fetch('https://raw.githubusercontent.com/softvenue/i18n/master/i18n.json')
+    .then((response) => response.json())
+    .then((data) => terms.push(...data));
+})();
+
+term.addEventListener('input', (e) => {
+  term.style.direction = e.target.value === '' ? 'rtl' : 'ltr';
+  if (e.target.value === '' || e.target.value.length < 3) {
+    results.innerHTML = '';
+    return;
+  }
+  const filteredResults = terms.filter((item) =>
+    item.term.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  results.innerHTML = filteredResults.length
+    ? filteredResults
+        .map(
+          (item) => `
+      <div class="result">
+        <h4 dir="ltr">🔤 ${item.term}</h4>
+        <ul>
+            ${
+              Array.isArray(item.wAR)
+                ? item.wAR.map((meaning) => `<li>${meaning}</li>`).join('')
+                : `<li>${item.wAR}</li>`
+            }
+        </ul>
+      </div>
+    `
+        )
+        .join('')
+    : `
+    <div class="no-results">
+      لا توجد نتيجة للبحث المطلوب: ${e.target.value}
+    </div>
+    `;
+});
+
+function lockFocus() {
+  term.focus();
+  term.select();
+}
+
+lockFocus();
+
+document.addEventListener('click', () => {
+  lockFocus();
+});
+
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'visible') lockFocus();
+});
